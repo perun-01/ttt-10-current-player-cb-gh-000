9f141d02ff26fce989ddaa5eea3910cf7a0264a2
Ranjeet Singh Sahni <singhranjeet5200@gmail.com>
2020-01-05 13:17:03 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,6 +1,6 @@
 def turn_count(board)
   counter = 0
-  board = ["O", " ", " ", " ", "X", " ", " ", " ", "X"]
+  board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
     board.each do |position|
       if board[counter] == "X" || board[counter] == "O"
         counter += 1
