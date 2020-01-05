0f65ad9327abff817322015d4795256c5c5677e6
Ranjeet Singh Sahni <singhranjeet5200@gmail.com>
2020-01-05 13:32:04 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,20 +1,20 @@
-def turn_count(board)
-  counter = 0
-    board.each do |position|
-      if board[counter] == "X" || board[counter] == "O"
-        counter += 1
-      end
-    return counter
-    end
-end
-
 # def turn_count(board)
-#   count = 0
-#   board.each do |index|
-#     index == "X" || index == "O" ? count += 1 : nil
-#   end
-#   count
+#   counter = 0
+#     board.each do |position|
+#       if board[counter] == "X" || board[counter] == "O"
+#         counter += 1
+#       end
+#     return counter
+#     end
 # end
+
+def turn_count(board)
+  count = 0
+  board.each do |index|
+    index == "X" || index == "O" ? count += 1 : nil
+  end
+  count
+end
 
 def current_player(board)
   turn_count(board).even? == true ? "X" : "O"
