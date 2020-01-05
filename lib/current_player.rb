5f403b39831cdf8e07d236110e13cfb29cf1726b
Ranjeet Singh Sahni <singhranjeet5200@gmail.com>
2020-01-05 13:31:31 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -8,6 +8,14 @@ def turn_count(board)
     end
 end
 
-def current_player(player)
+# def turn_count(board)
+#   count = 0
+#   board.each do |index|
+#     index == "X" || index == "O" ? count += 1 : nil
+#   end
+#   count
+# end
 
+def current_player(board)
+  turn_count(board).even? == true ? "X" : "O"
 end
