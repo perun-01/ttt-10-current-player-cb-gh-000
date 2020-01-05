83752e955f4feac4d5f1197dd2832c0b552ec6e4
Ranjeet Singh Sahni <singhranjeet5200@gmail.com>
2020-01-05 13:34:11 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,13 +1,3 @@
-# def turn_count(board)
-#   counter = 0
-#     board.each do |position|
-#       if board[counter] == "X" || board[counter] == "O"
-#         counter += 1
-#       end
-#     return counter
-#     end
-# end
-
 def turn_count(board)
   count = 0
   board.each do |index|
